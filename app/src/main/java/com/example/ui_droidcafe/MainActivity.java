4011089be80253b8103d7b6d9c96314f582186d6
kerish kumar <31753397+skerish@users.noreply.github.com>
2019-07-08 17:22:15 +0530

    Commit msg

---
+++ app/src/main/java/com/example/ui_droidcafe/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.ui_droidcafe;
+
+import android.app.job.JobInfo;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public String mOrderMessage;
+    public static final String EXTRA_MESSAGE = BuildConfig.APPLICATION_ID;
+
+    Boolean flag = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (flag){
+                    Intent intent = new Intent(getApplicationContext(), OrderActivity.class);
+                    intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                    startActivity(intent);
+                }
+                else{
+                    Snackbar.make(view, "Please select an order first!", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void displayToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDonutOrder(View view){
+        flag = true;
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void showFroYoOrder(View view){
+        flag = true;
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void showIceCreamOrder(View view){
+        flag = true;
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
+    }
+
+}
---
+++ app/src/main/java/com/example/ui_droidcafe/OrderActivity.java
@@ -0,0 +1,99 @@
+package com.example.ui_droidcafe;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * The reason why you need the AdapterView is because you need an adapter—specifically
+ * an ArrayAdapter—to assign the array to the Spinner. An adapter connects your data—in this
+ * case, the array of spinner items—to the Spinner.
+ */
+
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        TextView textView = findViewById(R.id.order_textView);
+
+        Intent intent = getIntent();
+        String msg = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        textView.setText(msg);
+
+        RadioButton default_btn = findViewById(R.id.sameday);
+        default_btn.setChecked(true);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null){
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        // Creates ArrayAdapter using the string array and default spinner layout.
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_choices, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner.
+        if (spinner != null){
+            spinner.setAdapter(arrayAdapter);
+        }
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()){
+            case R.id.sameday:
+                if(checked){
+                    displayToast(getString(R.string.sameday));
+                }
+                break;
+            case R.id.nextday:
+                if (checked){
+                    displayToast(getString(R.string.nextday));
+                }
+                break;
+            case R.id.pickUp:
+                if (checked){
+                    displayToast(getString(R.string.pickup));
+                }
+                break;
+        }
+    }
+
+    public void displayToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * The <?> is a Java type wildcard, enabling the method to be flexible enough to accept
+     * any type of AdapterView as an argument.
+     */
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        // To retrieve the user's selected item in the Spinner, use getItemAtPosition().
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
+
+}
